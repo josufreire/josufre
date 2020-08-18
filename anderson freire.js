b049d1f69d6815b7b6ab6410c607c108ea64f9d5
josufreire <69046131+josufreire@users.noreply.github.com>
2020-08-18 15:34:45 -0500

    Add files via upload

---
+++ anderson freire.js
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>lists escolar</title>
+    <link rel="stylesheet" type="text/css" href="">
+</head>
+<nav>
+	<ul>
+		<li>inicio</li>
+		<li>contacto</li>
+	</ul>
+</nav>
+    <body>
+    	<h1>cuadernos</h1>
+    	<h2>sacapuntas</h2>
+    	<h3>lapices</h3>
+    	<p>para escribir, dibujar o pintar que consiste en una barra delgada y larga generalmente de madera, con una mina cilíndrica fina de grafito.<b> HTML</b> </p>
+<iframe src="https://www.youtube.com/watch?v=wPU-OVDFy7I"></iframe>    	
